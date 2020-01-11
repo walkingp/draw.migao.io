1f1c428424c861ad24feb826e9abf4b0c9da942a
米高哥 <qc_69799f1c597a48e2b692280097438c7a@git.code.tencent.com>
2020-01-11 16:39:24 +0800

    Initialization

---
+++ js/award.js
@@ -0,0 +1,101 @@
+window.award = [{
+        name: 'Rocket Science',
+        award: 'Rocket Science胶衣（价值1300元）',
+        count: 2,
+    },
+    {
+        name: 'Rocket Science',
+        award: 'Rocket Science泳镜',
+        count: 5,
+    },
+    {
+        name: 'Rocket Science',
+        award: 'Rocket Science铁三背包',
+        count: 2,
+    },
+    {
+        name: 'Rocket Science',
+        award: 'Rocket Science号码带',
+        count: 5,
+    },
+    {
+        name: 'XTERRA',
+        award: 'XTERRA棒球帽',
+        count: 5,
+    },
+    {
+        name: 'XTERRA',
+        award: 'XTERRA号码带',
+        count: 5,
+    },
+    {
+        name: 'XTERRA',
+        award: 'XTERRA速干头⼱',
+        count: 5,
+    },
+    {
+        name: '罗赛洛',
+        award: '罗赛洛运动水壶',
+        count: 5,
+    },
+    {
+        name: '罗赛洛',
+        award: '罗赛洛蛋白粉',
+        count: 3,
+    },
+    {
+        name: 'Wizzo',
+        award: 'Wizzo污师鞋子洗涤剂',
+        count: 10,
+    },
+    {
+        name: '有羊',
+        award: '有羊手工皂',
+        count: 10,
+    },
+    {
+        name: '有羊',
+        award: '有羊面膜',
+        count: 10,
+    },
+    {
+        name: '岐力信息',
+        award: 'Nike Next%一双',
+        count: 1,
+    },
+    {
+        name: 'Nike',
+        award: 'Nike跑百巷袋子',
+        count: 20,
+    },
+    {
+        name: 'Nike',
+        award: 'Nike跑百巷纪念T恤',
+        count: 10,
+    },
+    {
+        name: '红斗士',
+        award: '红斗士压片糖果',
+        count: 10,
+    },
+    {
+        name: '贴力',
+        award: '贴力肌效贴',
+        count: 10,
+    },
+    {
+        name: 'Geroge Shi',
+        award: '萨洛蒙T恤',
+        count: 10,
+    },
+    {
+        name: '李懿琛',
+        award: 'Nike上海腔调限量款跑鞋女款38码',
+        count: 10,
+    },
+    {
+        name: '刚戈',
+        award: 'NB 运动紧身裤男M',
+        count: 10,
+    },
+]
